06f82952b505f26b71536d08836ea760ec6e2cce
Dzaki988 <dzakiabdurrohman29@gmail.com>
2024-12-21 20:50:33 +0700

    Create Motorku.java

---
+++ Motorku.java
@@ -0,0 +1,16 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Motorku {
+    public static void main(String[] args) {
+        System.out.println("Hobiku bermain motor");
+        
+        int x = 32000000;
+        double y = 1.5;
+        int xminus = -4000;
+        String motor = "aerox";
+        
+        System.out.println("motorku " + motor);
+        System.out.println("harganya" + x );
+    }
+}
